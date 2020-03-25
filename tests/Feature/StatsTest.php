a6734fafc3a0543c05525c20c2b50aa7a2088f20
Billy <billybofh@gmail.com>
2020-03-25 10:36:27 +0000

    WIP

---
+++ tests/Feature/StatsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StatsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function we_can_get_hourly_()
+    {
+
+    }
+}
